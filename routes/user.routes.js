8d6abc58173b2dc83155df55ac76e151ee2af42f
Dan Tkachuk <d.tkachuk@yahoo.com>
2022-04-17 12:23:02 +0300

    Fix user get method

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,10 +12,6 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {});
-
-router.post('/:id', async (req, res) => {});
-
 router.patch('/:id', async (req, res) => {});
 
 module.exports = router;
