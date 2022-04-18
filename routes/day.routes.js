4b0b3fcbf3ce6c9885a0f12d7264afe2da43ffdf
Dan Tkachuk <d.tkachuk@yahoo.com>
2022-04-18 10:48:34 +0300

    Add get days method

--- routes/day.routes.js
+++ routes/day.routes.js
@@ -7,14 +7,14 @@ const auth = require('../middleware/auth.middleware');
 router.post('/', auth, async (req, res) => {
   try {
     if (!req.query.date) {
-      return res.status(400).json({message: "Send date query"})
+      return res.status(400).json({ message: 'Send date query' });
     }
     const day = await Day.findOne({
       userId: req.user._id,
       date: req.query.date
     });
     if (day) {
-      res.status(200).json({message: "Date exists"});
+      res.status(200).json({ message: 'Date exists' });
     } else {
       await Day.create({
         date: dayjs().format('DD/MM/YYYY'),
@@ -22,20 +22,27 @@ router.post('/', auth, async (req, res) => {
         userId: req.user._id,
         habitStatusId: []
       });
-      res.status(201).json({message: "New date created"});
+      res.status(201).json({ message: 'New date created' });
     }
   } catch (error) {
     res.status(500).json({ message: 'Internal server error. Try again later' });
   }
 });
 
-router.get('/:userId', auth, async (req,res) => {
+router.get('/', auth, async (req, res) => {
   try {
-    
+    const days = await Day.find({ userId: req.user._id });
+    res.status(200).json({
+      days: days.map((day) => ({
+        date: day.date,
+        isPerfect: day.isPerfect,
+        habitStatusId: day.habitStatusId
+      }))
+    });
   } catch (error) {
-    
+    res.status(500).json({ message: 'Internal server error. Try again later' });
   }
-})
+});
 
 router.patch('/:date', async (req, res) => {});
 
